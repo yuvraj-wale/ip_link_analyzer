6964b936561b893f9081d190e2c217fa194f0898
yuvraj wale <yuvrajwale16@gmail.com>
2024-03-18 13:14:35 +0530

    refsac

--- settings.py
+++ settings.py
@@ -2,7 +2,7 @@
 
 # Packet capture settings
 CAPTURE_FILTER = "ip"
-CAPTURE_COUNT = 10 # number of packets, set to 0 for infinite capture
+CAPTURE_COUNT = 0 # number of packets, set to 0 for infinite capture
 CAPTURE_SAVE_TO_FILE = True
 CAPTURE_FILE_PATH = "captures/capture.pcap"
 CAPTURE_TIMEOUT = None  # No timeout by default for infinite capture
