f7b36a01a6f04e6f7e18cbfb1db23b9f56ad78ac
Yuvraj Wale <97686433+yuvraj-wale@users.noreply.github.com>
2024-03-22 12:14:55 +0530

    Update settings.py

--- settings.py
+++ settings.py
@@ -7,5 +7,5 @@ CAPTURE_SAVE_TO_FILE = True
 CAPTURE_FILE_PATH = "captures/capture.pcap"
 CAPTURE_TIMEOUT = None  # No timeout by default for infinite capture
 GEOLITE2_DB_PATH = '/Users/yuvraj/Downloads/GeoLite2-Country_20240312/GeoLite2-Country.mmdb' # download and set path/to/GeoLite2-Country database
-TARGET_COUNTRIES = ['United States', 'Canada'] # configurable target countries
+TARGET_COUNTRIES = [('United States', 'Canada'), ('France', 'Germany')] # configurable target countries
 
